5251f5a5d7064660dce2e02ac720826d6a3b5cda
franklin rodriguez <franklinserif@gmail.com>
2024-07-15 14:46:27 -0400

    feat(javascript log): :sparkles: add basic game logic

---
+++ main.js
@@ -0,0 +1,13 @@
+(() => {
+    const time = 30;
+    const movement = 20;
+    const movementBar = 20;
+
+    /** movement is used for margin */
+    const width = document.documentElement.clientWidth - movement;
+    const height = document.documentElement.clientHeight - movement;
+
+    let controlGame;
+    let player1;
+    let player2;
+})()
